ca7d50281def2ea7ae0f171a32b8caa1edc2b959
AbdullahBinMunawer <abdullahbinmunawer@gmail.com>
2021-01-24 14:12:42 +0300

    setup project

---
+++ index.py
@@ -0,0 +1,12 @@
+
+
+from InnerLayers.DomainLayer.DomainSpecificLanguage.QuestionStatus import QuestionStatus
+from InnerLayers.DomainLayer.DomainSpecificLanguage.Time import Time
+from InnerLayers.DomainLayer.DomainSpecificLanguage.UUID import UUID
+# from InnerLayers.DomainLayer.DomainSpecificLanguage.UUID import UUID
+from datetime import datetime
+import time
+
+print('hi')
+
+
