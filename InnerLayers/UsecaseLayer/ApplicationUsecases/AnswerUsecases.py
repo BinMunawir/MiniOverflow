d53077781db7e2c5ccda6338b0067ee3f9a97258
AbdullahBinMunawer <abdullahbinmunawer@gmail.com>
2021-01-25 10:33:41 +0300

    impl-usecases: hard delete answer

--- InnerLayers/UsecaseLayer/ApplicationUsecases/AnswerUsecases.py
+++ InnerLayers/UsecaseLayer/ApplicationUsecases/AnswerUsecases.py
@@ -21,3 +21,11 @@ def softDelete(db: Repositories, answerDTO: AnswerDTO) -> None:
     answer: Answer = db.answerRepository.fetch([answerDTO.answerID])[0]
     answer.changeStatus(AnswerStatus.DELETED())
     db.answerRepository.update(answer)
+
+
+def hardDelete(db: Repositories, answerDTO: AnswerDTO) -> None:
+    # implemented inefficiently
+    db.answerRepository.delete(answerDTO.answerID)
+    comments: [] = db.commentRepository.fetch(filteredByAnswerIDs=[answerDTO.answerID])
+    for c in comments:
+        pass  # hard delete comment
