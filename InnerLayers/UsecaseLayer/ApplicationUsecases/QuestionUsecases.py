d19f3be070974fbeb6f1c8807f29a48b80e057ea
AbdullahBinMunawer <abdullahbinmunawer@gmail.com>
2021-01-27 08:55:42 +0300

    impl-usecases: get question/s (no filteration)

--- InnerLayers/UsecaseLayer/ApplicationUsecases/QuestionUsecases.py
+++ InnerLayers/UsecaseLayer/ApplicationUsecases/QuestionUsecases.py
@@ -1,4 +1,5 @@
 from InnerLayers.DomainLayer.DomainModels.Question import Question
+from InnerLayers.DomainLayer.DomainSpecificLanguage.UUID import UUID
 from InnerLayers.RepositoriesLayer.Repositories import Repositories
 from InnerLayers.UsecaseLayer.DataTrnsferObjects.QuestionDTO import QuestionDTO
 from InnerLayers.UsecaseLayer.Services.Services import Services
@@ -8,3 +9,14 @@ def askQuestion(questionDTO: QuestionDTO) -> None:
     uuid = Services.uuidGenerator.generate()
     question = Question(uuid, questionDTO.title, questionDTO.body, questionDTO.tags)
     Repositories.questionRepository.save(question)
+
+
+def getQuestions() -> list:
+    questions: list = Repositories.questionRepository.fetch()
+    return QuestionDTO.toListOfDTO(questions)
+
+
+def getQuestion(questionID: UUID) -> QuestionDTO:
+    question: list = Repositories.questionRepository.fetch(filteredByUUIDs=[questionID])[0]
+    return QuestionDTO.toDTO(question)
+
