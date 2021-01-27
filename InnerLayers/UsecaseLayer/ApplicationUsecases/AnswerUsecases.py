bb25e4cde0e924f8e2b569f8975bfcd72673b90d
AbdullahBinMunawer <abdullahbinmunawer@gmail.com>
2021-01-27 09:09:36 +0300

    impl-usecases: soft delete answer

--- InnerLayers/UsecaseLayer/ApplicationUsecases/AnswerUsecases.py
+++ InnerLayers/UsecaseLayer/ApplicationUsecases/AnswerUsecases.py
@@ -1,4 +1,5 @@
 from InnerLayers.DomainLayer.DomainModels.Answer import Answer
+from InnerLayers.DomainLayer.DomainSpecificLanguage.AnswerStatus import AnswerStatus
 from InnerLayers.DomainLayer.DomainSpecificLanguage.UUID import UUID
 from InnerLayers.RepositoriesLayer.Repositories import Repositories
 from InnerLayers.UsecaseLayer.DataTrnsferObjects.AnswerDTO import AnswerDTO
@@ -14,3 +15,9 @@ def answerQuestion(questionID: UUID, answerDTO: AnswerDTO) -> None:
 def getAnswersOfQuestion(questionID: UUID) -> list:
     answers = Repositories.answerRepository.fetch(filteredByQuestionIDs=[questionID])
     return AnswerDTO.toListOfDTO(answers)
+
+
+def softDeleteAnswer(answerID: UUID) -> None:
+    answer: Answer = Repositories.answerRepository.fetch(filteredByUUIDs=[answerID])[0]
+    answer.changeStatus(AnswerStatus.DELETED())
+    Repositories.answerRepository.update(answer)
