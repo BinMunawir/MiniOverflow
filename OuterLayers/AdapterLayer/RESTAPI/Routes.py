010e214ca4599d983ddf8f2db34e4e93add48c18
AbdullahBinMunawer <abdullahbinmunawer@gmail.com>
2021-01-27 10:04:25 +0300

    impl-endpoints: GET: /api/questions

--- OuterLayers/AdapterLayer/RESTAPI/Routes.py
+++ OuterLayers/AdapterLayer/RESTAPI/Routes.py
@@ -3,12 +3,14 @@ from OuterLayers.AdapterLayer.RESTAPI.Endpoints.GetQuestions import GetQuestions
 from OuterLayers.AdapterLayer.RESTAPI.HttpRequest import HttpRequest
 from OuterLayers.AdapterLayer.RESTAPI.HttpResponse import HttpResponse
 
-endpoints = [
-]
-
 
 def router(request: HttpRequest) -> HttpResponse:
-    for e in endpoints:
-        pass
+    if request.path[0] != '/': request.path = '/'+request.path
+    if request.path[-1] != '/': request.path = request.path+'/'
+
+    if request.method == "GET" and request.path == "/api/questions/":
+        endpoint: GetQuestions = GetQuestions(request)
+        response = endpoint.handle()
+        return response
     return HttpResponse(404, {'Content-Type': 'application/json'}, json.dumps({'code': 1111, 'msg': 'Source Not Found'}))
 
