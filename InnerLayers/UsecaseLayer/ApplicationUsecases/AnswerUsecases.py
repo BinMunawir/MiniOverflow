8f44abed638b9d8d1bfe6087b57fa124cf239eb3
AbdullahBinMunawer <abdullahbinmunawer@gmail.com>
2021-01-27 09:25:29 +0300

    impl-usecases: hard delete answer

--- InnerLayers/UsecaseLayer/ApplicationUsecases/AnswerUsecases.py
+++ InnerLayers/UsecaseLayer/ApplicationUsecases/AnswerUsecases.py
@@ -1,7 +1,9 @@
 from InnerLayers.DomainLayer.DomainModels.Answer import Answer
+from InnerLayers.DomainLayer.DomainModels.Comment import Comment
 from InnerLayers.DomainLayer.DomainSpecificLanguage.AnswerStatus import AnswerStatus
 from InnerLayers.DomainLayer.DomainSpecificLanguage.UUID import UUID
 from InnerLayers.RepositoriesLayer.Repositories import Repositories
+from InnerLayers.UsecaseLayer.ApplicationUsecases.CommentUsecases import deleteComment
 from InnerLayers.UsecaseLayer.DataTrnsferObjects.AnswerDTO import AnswerDTO
 from InnerLayers.UsecaseLayer.Services.Services import Services
 
@@ -21,3 +23,12 @@ def softDeleteAnswer(answerID: UUID) -> None:
     answer: Answer = Repositories.answerRepository.fetch(filteredByUUIDs=[answerID])[0]
     answer.changeStatus(AnswerStatus.DELETED())
     Repositories.answerRepository.update(answer)
+
+
+def hardDeleteAnswer(answerID: UUID) -> None:
+    answer: Answer = Repositories.answerRepository.fetch(filteredByUUIDs=[answerID])[0]
+    answerComments: list = answer.comments
+    for e in answerComments:
+        c: Comment = e
+        deleteComment(c.commentID)
+    Repositories.answerRepository.delete(answerID)
