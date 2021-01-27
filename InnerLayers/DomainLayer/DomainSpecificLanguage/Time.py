3a5210b0cf2af596629e151d8a5c73601a6fe956
AbdullahBinMunawer <abdullahbinmunawer@gmail.com>
2021-01-27 13:47:56 +0300

    fix-DTO: convert time input to float

--- InnerLayers/DomainLayer/DomainSpecificLanguage/Time.py
+++ InnerLayers/DomainLayer/DomainSpecificLanguage/Time.py
@@ -1,17 +1,16 @@
-
-
 from datetime import datetime
 from time import time
 
+
 class Time:
     def __init__(self, unixTime=time()):
-        self.UNIX_TIME = unixTime
-        self.YEAR = datetime.fromtimestamp(unixTime).year
-        self.MONTH = datetime.fromtimestamp(unixTime).month
-        self.DAY = datetime.fromtimestamp(unixTime).day
-        self.HOUR = datetime.fromtimestamp(unixTime).hour
-        self.MINUTE = datetime.fromtimestamp(unixTime).minute
-        self.SECOND = datetime.fromtimestamp(unixTime).second
+        self.UNIX_TIME = float(unixTime)
+        self.YEAR = datetime.fromtimestamp(self.UNIX_TIME).year
+        self.MONTH = datetime.fromtimestamp(self.UNIX_TIME).month
+        self.DAY = datetime.fromtimestamp(self.UNIX_TIME).day
+        self.HOUR = datetime.fromtimestamp(self.UNIX_TIME).hour
+        self.MINUTE = datetime.fromtimestamp(self.UNIX_TIME).minute
+        self.SECOND = datetime.fromtimestamp(self.UNIX_TIME).second
 
     def isLessThan(self, another):
         return self.UNIX_TIME < another.UNIX_TIME
---
+++ OuterLayers/AdapterLayer/RESTAPI/Endpoints/AskQuestion.py
@@ -0,0 +1,20 @@
+import json
+
+from InnerLayers.DomainLayer.DomainSpecificLanguage.UUID import UUID
+from InnerLayers.RepositoriesLayer.Repositories import Repositories
+from InnerLayers.UsecaseLayer.ApplicationUsecases.QuestionUsecases import getQuestions
+from InnerLayers.UsecaseLayer.DataTrnsferObjects.QuestionDTO import QuestionDTO
+from OuterLayers.AdapterLayer.RESTAPI.Endpoint import Endpoint
+from OuterLayers.AdapterLayer.RESTAPI.HttpRequest import HttpRequest
+from OuterLayers.AdapterLayer.RESTAPI.HttpResponse import HttpResponse
+
+
+class AskQuestion(Endpoint):
+    def __init__(self, request: HttpRequest):
+        super(GetQuestions, self).__init__(request)
+
+    def handle(self) -> HttpResponse:
+        result = getQuestions()
+        result = QuestionDTO.toListOfMap(result)
+        response: HttpResponse = HttpResponse(200, {'Content-Type': 'application/json'}, json.dumps(result))
+        return response
